8601551ab9397e969b02b8b7c270be7b847392e2
Davi Costa <davim.costa@outlook.com>
2024-02-17 01:15:21 -0300

    Protótipo da tela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,56 @@
-import Image from "next/image";
+"use client"
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  const array = [100, 97, 95, 93, 90, 87, 85, 83, 80, 77, 75, 73, 70, 67, 65, 63, 60, 57, 55, 53, 50, 47, 45, 43, 40, 37, 35, 33, 30, 27, 25, 23, 20, 17, 15, 13, 10]
+
+
+  function discoverHeight(elementId: string) {
+    
+    const myElement = document.getElementById(`${elementId}`);
+
+    if (myElement) {
+      const { bottom } = myElement.getBoundingClientRect();
+      const pageHeight = window.innerHeight;
+      const elementDistance = pageHeight - bottom;
+
+      console.log('Distância do elemento ao fundo da página:', elementDistance, 'pixels');
+    }    
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className={styles.container}>
+        <aside className={styles.ruler}>
+        {array.map((number) => {
+            let content;
+            if (number % 10 === 0) {
+              content = number;
+            } else {
+              content = '-';
+            }
+            return (
+                <>
+                {content != '-' ? 
+                   <p 
+                   id={number.toString()}
+                   key={number}
+                   onClick={() => discoverHeight(number.toString())}
+                 >
+                   {content}
+                 </p>
+                : 
+                <span>
+                 &mdash;	
+                </span>
+                }
+                </>
+            );
+          })}
+        </aside>
+        <div className={styles.water}></div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
